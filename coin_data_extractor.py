64510beb500014b0f3add2e832329f2c48c9e606
a-pellissier <achillepellissier@gmail.com>
2021-04-26 18:08:31 +0200

    working python data extractor

---
+++ coin_data_extractor.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+import csv
+import sys
+
+def coin_data_extractor(year_start, month_start, day_start, year_end, month_end, day_end):
+    #This is the main api url
+    base_url = "http://api.coingecko.com/api/v3"
+
+    #We start by reading all the desired coins, and storing that in a list
+    with open('coin_list.csv', newline=None) as f:
+        reader = csv.reader(f)
+        coin_list=list(reader)
+
+    start_date = datetime.datetime(year_start, month_start, day_start)
+    end_date = datetime.datetime(year_end, month_end, day_end)
+    params_dict = {"vs_currency" : "usd",
+                    "from" : datetime.datetime.timestamp(start_date),
+                    "to" : datetime.datetime.timestamp(end_date)}
+    
+    for coin_id in coin_list[0]:
+        request_url = f"/coins/{coin_id}/market_chart/range"
+        response = requests.get(base_url + request_url,
+                            params = params_dict)
+        if response.status_code != 200:
+            print(f"Error on {coin_id}")
+        coin_data = response.json()
+        coin_data_df = pd.DataFrame(coin_data["market_caps"])
+        coin_data_df.to_csv(f"coin_data/{coin_id}", index = False, header = False)
+        print(f"Successfully saved {coin_id} data")
+
+if __name__ == "__main__":
+    coin_data_extractor(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6]))
